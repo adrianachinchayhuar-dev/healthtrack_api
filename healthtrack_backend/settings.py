659e38152acfab38d5315dea93edd9859a81ac5c
Adriana Chinchayhuara <adriana.chinchayhuar@tecsup.edu.pe>
2026-05-09 18:45:27 -0500

    Guardar cambios de la lógica del proyecto antes de sincronizar

--- healthtrack_backend/settings.py
+++ healthtrack_backend/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'corsheaders',
+    'django_filters',
+
+    # Local apps
+    'patients',
+    'doctors',
 ]
 
 MIDDLEWARE = [
