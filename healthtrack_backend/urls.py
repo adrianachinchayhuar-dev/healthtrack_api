d7deab1e690dc08b6719d60ea300075519209048
Adriana Chinchayhuara <adriana.chinchayhuar@tecsup.edu.pe>
2026-05-09 16:47:45 -0500

    feat: configure URL routing for API endpoints

--- healthtrack_backend/urls.py
+++ healthtrack_backend/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # Opcional, pero no se usará para gestión
+    # Endpoints de la API
+    path('api/doctors/', include('doctors.urls')),
+    path('api/patients/', include('patients.urls')),
 ]
